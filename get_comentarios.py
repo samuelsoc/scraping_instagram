a1e4113c195cda18e908e79f5bf55295239e1872
samuelsoc <samuelrg.sna@gmail.com>
2021-10-07 16:03:12 -0300

    respuestas

--- get_comentarios.py
+++ get_comentarios.py
@@ -18,7 +18,6 @@ ruta = os.getcwd()+ '/comentariospost'
 os.mkdir(ruta)
 
 idpostt = pd.read_csv('listado_id_comentarios.csv')
-#idpostt = idpostt.head(2)
 idpostt =  idpostt['shortcode'].to_list()
 
 
@@ -34,8 +33,8 @@ def scrape_comentario(idpost):
         fecha.append(comment.created_at_utc)
         for answer in comment.answers:
             comments_from_loop_including_answers.append(answer.text)
-            user.append(comment.owner)
-            fecha.append(comment.created_at_utc)
+            user.append(answer.owner)
+            fecha.append(answer.created_at_utc)
             
     df = pd.DataFrame({'usuario':user,
                        'comentario':comments_from_loop_including_answers,
@@ -45,7 +44,8 @@ def scrape_comentario(idpost):
     print(df)
     df.to_csv(ruta +'/'+f'comentarios_post_{idpost}.csv')
     sleep(random.randrange(0,30))
-    
+
+ 
 for idpost in idpostt:
     print(idpost)
     scrape_comentario(idpost)
